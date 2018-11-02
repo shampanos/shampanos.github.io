cad88df1c4181cca1f0ed18d0e7230e6694711f5
Bram van den Heuvel <shampanos@hotmail.com>
2018-11-02 15:55:20 +0000

    Updated 'js/custom.js' via CloudCannon

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
 
-    router = new Navigo(null, true, '?');
-    router.on('/category/:id/', function (params) {
-        $('#' + params.id).modal('show')
-    });
-    router.resolve();
+      // queryStrip
+      function queryStrip(string) {
+        string = string.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + string + '=([^&#]*)'),
+            results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ''));
+      }
+    
+      // Show bootstrap modal on load
+      // If the modal id="terms", you can show it on page load by appending `?modal=terms` to the URL
+      var modalString = queryStrip('category'),
+          modalToShow = '#' + modalString;
+      if (modalString !== '') {
+        $(modalToShow).modal('show');
+      }
 
+    $('.portfolio-link').click(function (e) {
+      e.preventDefault();
+      $(event.target).modal('show')
+    });
 });
